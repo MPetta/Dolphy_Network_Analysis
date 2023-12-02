1c069dac642953051dd2a5f5faf5667015e4ac5c
Marc Petta <MPetta@users.noreply.github.com>
2023-12-02 08:34:49 -0800

    Update app.py

--- app.py
+++ app.py
@@ -21,10 +21,8 @@ server = app.server
 app.title = "Musician Network - Eric Dolphy"
 
 # read data
-df = pd.read_csv("ericDolphyDiscog1.csv", encoding='utf_8')
-
+newData = pd.read_csv("ericDolphyDiscog1.csv", encoding='utf_8')
 K = 0.009 # updated by slider
-newData = df
 
 #########################################################################################################
 def network_graph(K, newData):
